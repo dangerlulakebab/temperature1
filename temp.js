b8ffb3ed4163aa3aecc1e245b92ae1300e1a6d51
kebab <101018207+dangerlulakebab@users.noreply.github.com>
2024-06-11 02:23:30 +0300

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,11 @@
+var kelvin = 293; // aboba
+celsius = kelvin - 273; // defines celsius
+var fahrenheit = celsius * (9/5) + 32; // asdasdasda
+fahrenheit = Math.floor(fahrenheit);
+var newton = Math.floor((celsius * (33/100)))
+
+
+console.log(`The temperature is ${celsius} degrees celsius`)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
+console.log(`The temperature is ${kelvin} degrees kelvin`)
+console.log(`in Newton its: ${newton}`)
